1b068139dbe66ef0aa54a8bd09d10a050db2143a
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-07 13:30:09 +0000

    removed docker

--- fedora.sh
+++ fedora.sh
@@ -3,10 +3,3 @@
 # Install dev tools
 /bin/bash -c "$(sudo dnf install gcc-c++)"
 /bin/bash -c "$(sudo dnf install valgrind)"
-
-# Install Docker
-/bin/bash -c"$(sudo dnf -y install dnf-plugins-core)"
-/bin/bash -c"$(sudo dnf config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo)"
-/bin/bash -c"$(sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin)"

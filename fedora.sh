64cb33764168f61a46a5675c55b7b7f48441d9d3
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-08 22:07:39 -0700

    moved sshd setup here

--- fedora.sh
+++ fedora.sh
@@ -3,3 +3,7 @@
 # Install dev tools
 /bin/bash -c "$(sudo dnf install gcc-c++)"
 /bin/bash -c "$(sudo dnf install valgrind)"
+
+# Setup sshd config and restart ssh
+/bin/bash -c "$(cp sshd_config /etc/ssh/)"
+/bin/bash -c "$(sudo service ssh restart)"

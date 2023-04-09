94b4c20c96dabfe110d8728d1de8464011b90c13
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-08 22:07:54 -0700

    removed nitch, sshd config, and fonts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/bash
 
 echo "Setting up Linux..."
 
@@ -20,14 +20,11 @@ ln -s .zshrc $HOME/.zshrc
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle --file ./Brewfile
+# brew tap homebrew/bundle
+# brew bundle --file ./Brewfile
 
 # Installs antigen
 /bin/bash -c "$(curl -L git.io/antigen > antigen.zsh)"
-
-# Installs nitch (system fetcher)
-/bin/bash -c "$(wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh && sh setup.sh)"
 
 # Install NeoVim
 if test ! $(which nvim); then
@@ -43,12 +40,3 @@ if [ -d "$TARGET_DIR" ]; then
 fi
 
 /bin/bash -c "$(git clone "$REPO_URL" "$TARGET_DIR")"
-
-# Install NerdFonts for p10k
-/bin/bash -c "$(git clone --depth=1 https://github.com/romkatv/nerd-fonts.git)"
-/bin/bash -c "$(cd nerd-fonts)"
-/bin/bash -c "$(./build 'Meslo/S/*')"
-
-# Setup sshd config and restart ssh
-/bin/bash -c "$(cp sshd_config /etc/ssh/)"
-/bin/bash -c "$(sudo service ssh restart)"

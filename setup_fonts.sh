a9630a611846f1247e9cd9b229fcd450ffaa20af
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-08 23:08:48 -0700

    renamed fle

--- setup_fonts.sh
+++
@@ -1,7 +0,0 @@
-!#!/bin/bash
-
-# Install NerdFonts for p10k
-/bin/bash -c "$(git clone --depth=1 https://github.com/romkatv/nerd-fonts.git)"
-/bin/bash -c "$(cd nerd-fonts)"
-/bin/bash -c "$(./build 'Meslo/S/*')"
-

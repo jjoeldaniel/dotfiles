a9a4c079c85441645aba2aac2f1d642c07752753
Joel Daniel Rico <joeldanielrico@gmail.com>
2024-01-23 07:08:26 -0800

    Now only uses Fish after Fish installation

--- install.py
+++ install.py
@@ -2,19 +2,20 @@ import shutil
 import pprint
 import os
 import subprocess
+from enum import Enum
 
 
-def cmd(args: str):
-    subprocess.run(
-        args,
-        stdout=subprocess.DEVNULL,
-        shell=True,
-        executable="/usr/bin/fish",
-    )
+class Shell(Enum):
+    BASH = "/bin/bash"
+    FISH = "/usr/bin/fish"
+
+
+def cmd(args: str, shell: Shell = Shell.BASH):
+    subprocess.run(args, stdout=subprocess.DEVNULL, shell=True, executable=shell.value)
 
 
 def fisher_which(command: str) -> bool:
-    return True if f"/{command}" in str(cmd("fisher list")) else False
+    return True if f"/{command}" in str(cmd("fisher list", shell=Shell.FISH)) else False
 
 
 def which(command: str, install_command: str | None = None) -> bool:
@@ -31,8 +32,9 @@ class Package:
 
     def install(self, install_command: str):
         if not which(self.command, install_command):
+            shell: Shell = Shell.FISH if "fish" in install_command else Shell.BASH
             try:
-                cmd(args=f"{install_command} {self.name}")
+                cmd(args=f"{install_command} {self.name}", shell=shell)
             except subprocess.CalledProcessError as e:
                 print(f"Installing command {e.cmd} failed with error {e.returncode}")
 
